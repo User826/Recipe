089cf975c9e5329223e4d13b760991c2e4d0c4b6
Danny Wu <dannywu826@gmail.com>
2023-06-02 22:40:45 -0400

    Added card functionality

---
+++ .history/recipe_project/pages/index_20230601052722.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import {UserButton, AdminButton} from "../components/websitebuttons"
+import Post from "../components/users"
+import styles from '../pages/index.module.css'
+import {PopupTrigger,Backdrop, Popup} from "../components/popup";
+
+
+export default function Useradmin() {
+    const [isLoggedIn, setIsLoggedIn] = useState("");
+    const [showPopup, setShowPopup] = useState(false);
+
+    function handleLoginClick() {
+        //this.setState({isLoggedIn: true});
+        setIsLoggedIn("user")
+      };
+    
+    function handleLogoutClick() {
+        //this.setState({isLoggedIn: false});
+        setIsLoggedIn("admin")
+      }
+    const handleParentClick = () => {
+      setShowPopup(false);
+    };
+    
+    
+    const isLoggedInVariable = isLoggedIn;
+    let button;
+
+    // if (isLoggedInVariable) {
+    //     button = <LogoutButton onClick={handleLogoutClick} />;
+    // } else {
+    //     button = <LoginButton onClick={handleLoginClick} />;
+    // }
+
+    console.log(isLoggedInVariable)
+
+    if (isLoggedInVariable == ""){
+        return <div>
+        {/* <Post></Post> */}
+        <UserButton onClick ={handleLoginClick} />        
+        <AdminButton onClick = {handleLogoutClick}/>
+        
+    </div>
+    }
+    else if (isLoggedInVariable == "user"){
+        return <div onClick = {handleParentClick}>
+                    <p>You are a user!</p>
+                    <p>Or are you an admin? <AdminButton onClick = {handleLogoutClick}> </AdminButton></p>
+                    <Post name="Username"></Post>
+                    <PopupTrigger></PopupTrigger>
+                </div>
+            
+                // <UserButton onClick = {handleLoginCLick}> </UserButton>
+        
+    }
+    else if (isLoggedInVariable == "admin"){
+        return  <div>
+                    <p>You are an admin!</p>
+                    <p>Or are you a user? <UserButton onClick = {handleLoginClick}> </UserButton></p>
+                    <Post name="Admin Username"></Post>
+                </div> 
+        
+    }
+    
+    // return <div>
+    //     {/* <Post></Post> */}
+    //     <UserButton onClick ={handleLoginClick}/>        
+    //     <AdminButton onClick = {handleLoginClick}/>
+        
+    // </div>
+  }
---
+++ .history/recipe_project_api/app_20230602033744.js
@@ -0,0 +1,233 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+var jsonParser = bodyParser.json()
+
+const app = express()
+app.use(cors(corsOptions));
+app.post('/', jsonParser, (req, res) => {
+
+
+    // console.log(req.get('Content-type'))
+    // console.log(typeof(req.body))
+    
+
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const uri = "mongodb+srv://Dannywu826:Momo826826@cluster0.sstc4pm.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+      client.connect(err => {
+        const collection = client.db("Recipe").collection("User");
+        // perform actions on the collection object
+       
+        collection.insertOne((req.body))
+        setTimeout(() => {client.close()}, 1500)
+
+      });
+
+    // if (err) throw err;
+    // var dbo = db.db("Recipe");
+    // var myobj = { name: "Company Inc", address: "Highway 37" };
+    // dbo.collection("User").insertOne(myobj, function(err, res) {
+    //     if (err) throw err;
+    //     console.log("1 document inserted");
+    //     db.close();
+    // });
+
+
+
+      res.status(200).send({ message: (req.body.title) + ' ' + (req.body.post) })
+})
+
+app.post('/confirm', jsonParser, (req, res) => {
+
+
+  console.log(req.get('Content-type'))
+  console.log(typeof(req.body))
+  
+
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb+srv://Dannywu826:Momo826826@cluster0.sstc4pm.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  var allowLogIn = false
+
+  var collection
+    client.connect(err => {
+      console.log(req.body.type)
+      console.log(req.body.name)
+      if (req.body.type == 'Username'){
+        collection = client.db("Recipe").collection("User");
+        
+      }
+      else if (req.body.type == 'Admin Username'){
+        collection = client.db("Recipe").collection("Admin");
+      }
+      // perform actions on the collection object
+     
+      // console.log(collection.find({title:req.body.title, post: req.body.post}))
+
+      const cursor = collection.find({username:req.body.name, password: req.body.password})
+
+      cursurArray = cursor.toArray().then(function(result){
+          console.log(result)
+          console.log(result.length==0)
+
+          if (result.length != 0){
+        
+            res.status(200).send({message: "You can log in!"})
+            
+      
+          }
+          else {
+            res.status(200).send({message: "Invalid login"})
+          }
+      })
+      // console.log(cursurArray)
+
+      // console.log(cursurArray.length)
+
+      
+
+      
+      // if(cursor.next() != null){
+      //   allowLogIn = true
+      // }
+      
+      setTimeout(() => {client.close()}, 1500)
+
+    });
+
+  // if (err) throw err;
+  // var dbo = db.db("Recipe");
+  // var myobj = { name: "Company Inc", address: "Highway 37" };
+  // dbo.collection("User").insertOne(myobj, function(err, res) {
+  //     if (err) throw err;
+  //     console.log("1 document inserted");
+  //     db.close();
+  // });
+
+
+    // if (allowLogIn == true){
+    //   res.status(200).send({message: "You can log in!"})
+
+    // }
+    // res.status(200).send({ message: (req.body.title) + ' ' + (req.body.post) })
+})
+
+app.post('/test', jsonParser, (req, res) => {
+
+
+  // console.log(req.get('Content-type'))
+  // console.log(typeof(req.body))
+  
+
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb+srv://Dannywu826:Momo826826@cluster0.sstc4pm.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  var allowLogIn = false
+
+  var collection
+    client.connect(err => {
+      // console.log(req.body.type)
+      // console.log(req.body.name)
+    
+      if (req.body.type == 'Username'){
+        collection = client.db("Recipe").collection("User");
+        
+      }
+      else if (req.body.type == 'Admin Username'){
+        collection = client.db("Recipe").collection("Admin");
+      }
+      // perform actions on the collection object
+     
+      // console.log(collection.find({title:req.body.title, post: req.body.post}))
+
+      const cursor = collection.find()
+
+      cursurArray = cursor.toArray().then(function(result){
+          // console.log(result)
+          // console.log(result.length==0)
+
+          if (result.length != 0){
+        
+            res.status(200).send({sendData:result})
+            
+      
+          }
+          else {
+            res.status(200).send({message: "Invalid login"})
+          }
+      })
+      // console.log(cursurArray)
+
+      // console.log(cursurArray.length)
+
+      
+
+      
+      // if(cursor.next() != null){
+      //   allowLogIn = true
+      // }
+      
+      // setTimeout(() => {client.close()}, 1500)
+      client.close()
+
+    });
+
+  // if (err) throw err;
+  // var dbo = db.db("Recipe");
+  // var myobj = { name: "Company Inc", address: "Highway 37" };
+  // dbo.collection("User").insertOne(myobj, function(err, res) {
+  //     if (err) throw err;
+  //     console.log("1 document inserted");
+  //     db.close();
+  // });
+
+
+    // if (allowLogIn == true){
+    //   res.status(200).send({message: "You can log in!"})
+
+    // }
+    // res.status(200).send({ message: (req.body.title) + ' ' + (req.body.post) })
+})
+
+app.post('/sample', jsonParser, (req, res) =>{
+  
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Dannywu826:Momo826826@cluster0.sstc4pm.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("Recipe").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+})
+
+
+app.listen(5000, () => {
+    console.log("Listening to port 5000")
+})
